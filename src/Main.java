2696245e2dc11a64604fb77c9ad48bbebcbde895
Andrey Tolokonnikov <andrey572@mail.ru>
2023-03-19 11:14:22 +0300

    commit number 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 6900;
-        int y = 20;
-        System.out.println(x / y);
+        int ticketPrice = 6900;
+        int bonusPerMile = 20;
+        System.out.println(ticketPrice / bonusPerMile);
     }
 }
